a57f7fc83754b9d68c8e928cbaf0e91beb54b219
Jean-Michel <jean-michel@lehker.net>
2021-01-27 12:04:08 -0800

    basic frontend

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,60 +1,32 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-card width="450" class="mx-auto mt-5">
+      <v-card-title class="pb-0">
+        <h1>PetTutor Control</h1>
+      </v-card-title>
+      <v-card-text> </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="info" @click="feed()">Manual Feed</v-btn>
+        <v-btn color="success">Start Schedule</v-btn>
+        <v-btn color="error">Stop Schedule</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "App",
+  data() {
+    return {
+      //
+    };
   },
-
-  data: () => ({
-    //
-  }),
+  methods: {
+    async feed() {
+      await this.axios.get("http://raspberrypi.home:8000/feed");
+    },
+  },
 };
 </script>
